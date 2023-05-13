e5b3f2c2d8bf82d00a2d8704b99defdec874a1e1
alicodesnow <132698972+alicodesnow@users.noreply.github.com>
2023-05-13 22:51:18 +0800

    Add files via upload

---
+++ js/buttonFilterNavMenu.js
@@ -0,0 +1,8 @@
+function kadiwaButtonFilter() {
+    var i = document.getElementById("buttonFilterCategory");
+    if (i.style.display === "block") {
+      i.style.display = "none";
+    } else {
+      i.style.display = "block";
+    }
+}
